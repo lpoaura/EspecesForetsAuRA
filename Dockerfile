928c5b7d4cf6738910954b26d5f159a49381ccdc
lpofredc <frederic.cloitre@lpo.fr>
2021-05-31 15:07:21 +0200

    feat: Dockerize app

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:current-alpine AS builder
+
+WORKDIR /app
+
+COPY package*.json /app/
+
+RUN npm install
+
+COPY ./ /app/
+
+RUN npm run build && ls -alh /app/dist/
+
+FROM nginx:alpine as serv
+
+COPY --from=builder /app/dist/ /usr/share/nginx/html
+
--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,25 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require("path");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");s
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        'cartes/foretaura' : '/',
-    outputDir: undefined,
-    assetsDir: 'assets',
-    runtimeCompiler: undefined,
-    productionSourceMap: false,
-    parallel: undefined,
-    css: undefined,
-    configureWebpack: config => {
-        if (process.env.NODE_ENV !== 'production') return;
-        return {
-            plugins: [
-                new PrerenderSPAPlugin({
-                    // Required - The path to the webpack-outputted app to prerender.
-                    staticDir: path.join(__dirname, 'dist'),
-                    // Required - Routes to render.
-                    routes: ['/', '/dept/01', '/dept/03', '/dept/07', '/dept/15', '/dept/26', '/dept/38', '/dept/42', '/dept/43', '/dept/63', '/dept/69', '/dept/73', '/dept/74'],
-                })
-            ]
-        }
-    }
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: undefined,
+  assetsDir: "assets",
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined,
+  // configureWebpack: config => {
+  //     if (process.env.NODE_ENV !== 'production') return;
+  //     return {
+  //         plugins: [
+  //             new PrerenderSPAPlugin({
+  //                 // Required - The path to the webpack-outputted app to prerender.
+  //                 staticDir: path.join(__dirname, 'dist'),
+  //                 // Required - Routes to render.
+  //                 routes: ['/', '/dept/01', '/dept/03', '/dept/07', '/dept/15', '/dept/26', '/dept/38', '/dept/42', '/dept/43', '/dept/63', '/dept/69', '/dept/73', '/dept/74'],
+  //             })
+  //         ]
+  //     }
+  // }
+};
